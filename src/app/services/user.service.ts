7af8e0c2f6320dc2d581346ac5d0310f86f5096a
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-11-10 14:21:17 -0500

    Added some of the backend for the blog

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
+import {User} from './../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private readonly baseUserURL = "http://localhost:3000/api/users/";
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUserURL);
+  }
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.baseUserURL, user);
+  }
+  removeUser(id: number): Observable<User> {
+    return this.http.delete<User>(this.baseUserURL + id);
+  }
+  getUser(id: string): Observable<User> {
+    return this.http.get<User>(this.baseUserURL + id);
+  }
+
 }
+
